255c24d904bc2a7e5343da0c31111f8755a303bf
flaviusdev <>
2020-06-23 11:23:53 +0300

    Add some code

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/users', 'UserController@getUsers');
+Route::get('/users/{user_id}', 'UserController@getUser');
+Route::get('/images/random', 'ImageController@getRandomImage');
+Route::get('/images/{image_id}', 'ImageController@getImage');
+
 Route::group(['middleware' => 'auth:api'], function() {
     Route::post('/users', 'UserController@addUser');
-    Route::get('/users', 'UserController@getUsers');
-    Route::get('/users/{user_id}', 'UserController@getUser');
     Route::put('/users/{user_id}', 'UserController@editUser');
     Route::delete('/users/{user_id}', 'UserController@deleteUser');
     Route::delete('/users/comments', 'UserController@addComment');
-    Route::get('/images/random', 'ImageController@getRandomImage');
     Route::put('/images/{image_id}', 'ImageController@editImage');
-    Route::get('/images/{image_id}', 'ImageController@getImage');
     Route::post('/images', 'ImageController@addImage');
     Route::post('/logout', 'UserController@logout');
 });
